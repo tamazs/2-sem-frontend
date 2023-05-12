f8f39e83538967d76ab1b8db9f6182fcaac4b989
Tamás Major <majortamas313@gmail.com>
2023-05-12 11:10:16 +0200

    added account settings

---
+++ src/components/SettingsForm.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="list-body">
+        <div class="task-top">
+            <button class="close"><h2 class="close-btn">X</h2></button>
+            <h2 class="title">Account settings</h2>
+        </div>
+        <div class="folder-body">
+            <input type="text" value="Mr Bean" placeholder="Name">
+            <input type="email" value="mrbean@gmail.com" placeholder="Email">
+            <input type="password" placeholder="Password">
+            <button class="edit-btn">Edit account</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.list-body {
+    height: 40rem;
+    width: 40rem;
+    position: absolute;
+    background-color: white;
+    padding: 0;
+    border: 2px solid black;
+}
+
+.title {
+    width: 40rem;
+    height: max-content;
+    background-color: #DADAD3;
+    text-align: center;
+    margin: 0;
+    padding: 0.3rem 0;
+    font-size: 1rem;
+    border-bottom: 2px solid black;
+    position: fixed;
+}
+
+h3 {
+    text-align: center;
+}
+
+.folder-body {
+    width: 100%;
+    height: 100%;
+    padding: 5rem 3rem;
+    display: flex;
+    flex-direction: column;
+}
+
+img {
+    height: 6rem;
+}
+
+a {
+    text-decoration: none;
+    color: black;
+}
+
+.close-btn {
+    margin: 0;
+    width: 2rem;
+    text-align: center;
+    padding: 0.2rem 0;
+    font-size: 1rem;
+}
+
+.close {
+    position: fixed;
+    z-index: 4;
+    background-color: #DADAD3;
+    margin: 0;
+    width: 2rem;
+    height: max-content;
+    padding: 0;
+    width: max-content;
+}
+
+.edit-btn {
+    background-color: #54B9AD;
+    color: white;
+    padding: 1rem 2rem;
+    width: 30%;
+    border: 2px solid black;
+}
+
+p {
+    max-width: 90%;
+    overflow-wrap: break-word;
+}
+
+input {
+    margin-bottom: 1.5rem;
+    width: 80%;
+    height: 2rem;
+    border: 1px solid black;
+}
+</style>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,13 +1,25 @@
 <template>
-    <div>
-
+    <div class="settings-body">
+        <SettingsForm/>
     </div>
 </template>
 
 <script setup>
+import SettingsForm from '../components/SettingsForm.vue';
+
 
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.settings-body {
+  position: relative;
+  background: #54B9AD;
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+  background-size: 105rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
